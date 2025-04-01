463915daa8ab8f913a4df0b2cbe74274fe409832
corentin <corentin.batard2003@gmail.com>
2025-04-01 22:16:47 +0200

    espace emoji log

--- utils/dbInit.js
+++ utils/dbInit.js
@@ -43,11 +43,11 @@ async function initializeDatabase() {
     const sql = await fs.readFile(initSqlPath, "utf8");
     // Exécution du script SQL sur la base de données.
     await promisePool.query(sql);
-    console.log("🗂️\x1b[32m Base de données initialisée avec succès. \x1b[0m");
+    console.log("🗂️ \x1b[32m Base de données initialisée avec succès. \x1b[0m");
   } catch (err) {
     // En cas d'erreur, affichage du message d'erreur dans la console.
     console.error(
-      "🗂️\x1b[31m Erreur lors de l'exécution du script SQL : \x1b[0m",
+      "🗂️ \x1b[31m Erreur lors de l'exécution du script SQL : \x1b[0m",
       err.message,
     );
     throw err;
