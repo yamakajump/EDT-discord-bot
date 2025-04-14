c9fa9f1f5d5c7054b40ee8a71a17d83e32b31947
yamakajump <corentin.batard2003@gmail.com>
2025-04-14 15:51:40 +0200

    Ajout de logs pour afficher l'état de l'enregistrement et les données fournies dans handleUserPhysique

--- logic/handlePhysiqueData.js
+++ logic/handlePhysiqueData.js
@@ -75,6 +75,8 @@ async function handleUserPhysique(
 
   let finalData = {};
 
+  console.log("enregistrer :", guerrier.enregistrer);
+  console.log("Données fournies :", providedData);
   // 2. Si "enregistrer" est false, on utilise uniquement les données fournies
   if (guerrier.enregistrer == 0) {
     finalData = { ...providedData };
@@ -91,9 +93,8 @@ async function handleUserPhysique(
         finalData[key] = providedData[key];
       }
     });
-    console.log("Données fournies :", providedData);
-    console.log("Données finales :", finalData);
   }
+  console.log("Données finales :", finalData);
 
   // 4. Mise à jour de la DB si l'utilisateur a choisi de sauvegarder (enregistrer === true)
   if (guerrier.enregistrer == 1) {
