274ce6996ff6c2a17c4950c45bade607f281406f
yamakajump <corentin.batard2003@gmail.com>
2025-04-17 10:36:57 +0200

    changement tabulation

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -54,7 +54,7 @@ module.exports = {
           .setName(newName)
           .then(() => {
             console.log(
-              `\x1b[38;5;5m📈  Compteur de membres mis à jour : \x1b[38;5;13m${member.guild.memberCount} membres 	\x1b[38;5;2m+1\x1b[0m`,
+              `\x1b[38;5;5m📈  Compteur de membres mis à jour : \x1b[38;5;13m${member.guild.memberCount} membres \x1b[38;5;2m+1\x1b[0m`,
             );
           })
           .catch((err) =>
